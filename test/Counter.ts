4bf0c04a118b4c7d4fda96aab14a37d614636b87
vibonnet <82974617+vibonnet@users.noreply.github.com>
2025-07-02 12:05:26 +0200

    update test

--- test/Counter.ts
+++ test/Counter.ts
@@ -1,8 +1,23 @@
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { ethers } from "hardhat";
 
+type Signers = {
+  owner: HardhatEthersSigner;
+  alice: HardhatEthersSigner;
+  bob: HardhatEthersSigner;
+};
+
 describe("Counter", function () {
-  it("empty test", async function () {
-    console.log("Cool! The test basic skeleton is running!");
+  let signers: Signers;
+
+  before(async function () {
+    const ethSigners: HardhatEthersSigner[] = await ethers.getSigners();
+    signers = { owner: ethSigners[0], alice: ethSigners[1], bob: ethSigners[2] };
+  });
+
+  it("should work", async function () {
+    console.log(`address of user owner is ${signers.owner.address}`);
+    console.log(`address of user alice is ${signers.alice.address}`);
+    console.log(`address of user bob is ${signers.bob.address}`);
   });
 });
