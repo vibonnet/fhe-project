85e7e11c298d4e2b1dfb8be6f3ec985c156059e0
vibonnet <82974617+vibonnet@users.noreply.github.com>
2025-07-02 12:40:08 +0200

    update with function test

--- test/Counter.ts
+++ test/Counter.ts
@@ -37,4 +37,30 @@ describe("Counter", function () {
     // Test the deployed address is valid
     expect(ethers.isAddress(counterContractAddress)).to.eq(true);
   });
+
+  it("count should be zero after deployment", async function () {
+    const count = await counterContract.getCount();
+    console.log(`Counter.getCount() === ${count}`);
+    // Expect initial count to be 0 after deployment
+    expect(count).to.eq(0);
+  });
+
+  it("increment the counter by 1", async function () {
+    const countBeforeInc = await counterContract.getCount();
+    const tx = await counterContract.connect(signers.alice).increment(1);
+    await tx.wait();
+    const countAfterInc = await counterContract.getCount();
+    expect(countAfterInc).to.eq(countBeforeInc + 1n);
+  });
+
+  it("decrement the counter by 1", async function () {
+    // First increment, count becomes 1
+    let tx = await counterContract.connect(signers.alice).increment(1);
+    await tx.wait();
+    // Then decrement, count goes back to 0
+    tx = await counterContract.connect(signers.alice).decrement(1);
+    await tx.wait();
+    const count = await counterContract.getCount();
+    expect(count).to.eq(0);
+  });
 });
