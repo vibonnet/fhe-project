3a457fbed9444efd8513d76469dc75366b034a28
vibonnet <82974617+vibonnet@users.noreply.github.com>
2025-07-02 12:03:41 +0200

    add test

---
+++ test/Counter.ts
@@ -0,0 +1,8 @@
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { ethers } from "hardhat";
+
+describe("Counter", function () {
+  it("empty test", async function () {
+    console.log("Cool! The test basic skeleton is running!");
+  });
+});
