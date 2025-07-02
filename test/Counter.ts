393f4e28f00bc55140ecea0caa560a55c31e2c50
vibonnet <82974617+vibonnet@users.noreply.github.com>
2025-07-02 12:13:18 +0200

    update test with deployment

--- test/Counter.ts
+++ test/Counter.ts
@@ -1,23 +1,40 @@
+import { Counter, Counter__factory } from "../types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
 type Signers = {
-  owner: HardhatEthersSigner;
+  deployer: HardhatEthersSigner;
   alice: HardhatEthersSigner;
   bob: HardhatEthersSigner;
 };
 
+async function deployFixture() {
+  const factory = (await ethers.getContractFactory("Counter")) as Counter__factory;
+  const counterContract = (await factory.deploy()) as Counter;
+  const counterContractAddress = await counterContract.getAddress();
+
+  return { counterContract, counterContractAddress };
+}
+
 describe("Counter", function () {
   let signers: Signers;
+  let counterContract: Counter;
+  let counterContractAddress: String;
 
   before(async function () {
     const ethSigners: HardhatEthersSigner[] = await ethers.getSigners();
-    signers = { owner: ethSigners[0], alice: ethSigners[1], bob: ethSigners[2] };
+    signers = { deployer: ethSigners[0], alice: ethSigners[1], bob: ethSigners[2] };
   });
 
-  it("should work", async function () {
-    console.log(`address of user owner is ${signers.owner.address}`);
-    console.log(`address of user alice is ${signers.alice.address}`);
-    console.log(`address of user bob is ${signers.bob.address}`);
+  beforeEach(async () => {
+    // Deploy a new instance of the contract before each test
+    ({ counterContract, counterContractAddress } = await deployFixture());
+  });
+
+  it("should be deployed", async function () {
+    console.log(`Counter has been deployed at address ${counterContractAddress}`);
+    // Test the deployed address is valid
+    expect(ethers.isAddress(counterContractAddress)).to.eq(true);
   });
 });
